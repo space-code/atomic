ac3cc1a1c0d00cbf9dc9ffc9db849c217bfba5b9
Nikita Vasilev <nv3212@gmail.com>
2023-06-18 17:57:06 +0400

    Create the initial project's structure

---
+++ Sources/Atomic/Atomic.swift
@@ -0,0 +1,10 @@
+//
+// Concurrency
+// Copyright © 2023 Space Code. All rights reserved.
+//
+
+public struct Atomic {
+    public private(set) var text = "Hello, World!"
+
+    public init() {}
+}
---
+++ Tests/AtomicTests/AtomicTests.swift
@@ -0,0 +1,16 @@
+//
+// Concurrency
+// Copyright © 2023 Space Code. All rights reserved.
+//
+
+@testable import Atomic
+import XCTest
+
+final class AtomicTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Atomic().text, "Hello, World!")
+    }
+}
